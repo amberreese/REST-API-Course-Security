2f6012393890207b7c93173bf65e7f24460906ce
amberreese <amber.reese@kinandcarta.com>
2021-02-17 11:08:26 -0600

    example with tokens

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-// Basic Authentication
+// JWT Token Authentication
 // Part of the course on "REST API Design Development & Management"
 // http://www.acloudfan.com
 
 var     express = require('express')
-var     basicauth = require(__dirname + '/basicauth')
-
+var     bodyParser = require('body-parser')
+var     jwtAuth = require(__dirname + '/tokens/jwtauth')
+var     jwtValidate = require(__dirname + '/tokens/validator')
 
 // Express app setup
 var app = express();
+app.use(bodyParser.json())
 var router = express.Router();
 
-// This is the passport middlewae function that get called first
-var  auth = basicauth.auth
-// Setup the route with basic authentication
-router.get('/private',auth,function(req, res){
+// This is the passport middlewae function tha get called first
+var  auth = jwtAuth.auth
+router.post('/token',auth,function(req, res){
+    res.send('token');
+});
+
+auth = jwtValidate.auth
+router.get('/private',auth,function(req,res){
     res.send('Access granted to private resource!!!')
 });
 
